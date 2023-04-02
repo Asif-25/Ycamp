aaebedbe8fd8835304ebd4d69ffc04d344919b63
Asif <rahmanasif158@gmail.com>
2023-04-03 00:44:30 +0530

    added author and styles

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -28,6 +28,7 @@ router.get('/new',isLoggedIn, (req, res) => {
 })
 router.post('/',isLoggedIn,validateCampground, catchAsync(async (req, res) => {
     const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
     await campground.save();
     req.flash('success','Successfully made a new Campground!');
     res.redirect(`/campgrounds/${campground._id}`)
@@ -38,7 +39,7 @@ router.get('/:id/edit',isLoggedIn, catchAsync(async (req, res) => {
 }))
 
 router.get('/:id',catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews');
+    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
     if (!campground){
         req.flash('error', "Campground Doesn't Exist!");
         return res.redirect('/campgrounds');
